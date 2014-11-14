eda1aae12f14bf6b84d6af8eda49255007e9f9fa
Kirali <b00901187@ntu.edu.tw>
2014-11-14 17:08:00 +0800

    fix initialize and testbench

--- exp3_testbench.v
+++ exp3_testbench.v
@@ -6,12 +6,12 @@ module testbench;
 //===============================================
 //============= Signal Declaration ==============
 	// signal in top module
-	reg		rest;
+	reg		reset;
 	reg		clk;
 
 	// Codec
 	wire	I2C_SCLK;
-	reg		I2C_SDAT;
+	wire	I2C_SDAT;
 	wire	AUD_XCK;
 	wire	AUD_BCLK;
 	wire	AUD_DACDAT;
@@ -27,7 +27,7 @@ module testbench;
 		.clk(clk),
 		.I2C_SCLK(I2C_SCLK),
 		.I2C_SDAT(I2C_SDAT),
-		.AUD_XCK(AUD_XCK)
+		.AUD_XCK(AUD_XCK),
 		.AUD_BCLK(AUD_BCLK),
 		.AUD_DACDAT(AUD_DACDAT),
 		.AUD_DACLRCK(AUD_DACLRCK),
@@ -49,21 +49,25 @@ module testbench;
         #(`CYCLE/2) clk = ~clk; 
     end
 
+    assign I2C_SDAT = (i % 9 == 0)? 0 : 1'bz;
+    
 	initial begin
         #0; // t = 0
         clk     = 1'b1;
-        reset   = 1'b0; 
+        reset   = 1'b1; 
 
-        #(`CYCLE) reset = 1'b1; // t = 1
-        #(`CYCLE) reset = 1'b0; // t = 2
+        #(`CYCLE) reset = 1'b0; // t = 1
+        #(`CYCLE) reset = 1'b1; // t = 2
 
 		#(0.001);
 		i = 1;
-		while(i <= END_CYCLE) begin
+		while(i <= `END_CYCLE) begin
 			#(`CYCLE);
-			if(i % 9 == 0)
-				I2C_SDAT = 1'b0;
+            i = i + 1;
 		end
+        
+        $display("-------------------------DONE------------------------\n");
+        $finish;
 	end
 
 endmodule
